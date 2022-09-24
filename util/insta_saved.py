b93996c1d00e897d689ddd706f0b8263d296781c
Marek Hummel <marek.hummel3@gmail.com>
2022-09-24 19:10:35 +0200

    Added downloader for instagram saved posts

---
+++ util/insta_saved.py
@@ -0,0 +1,53 @@
+from glob import glob
+from os.path import expanduser
+from platform import system
+from sqlite3 import OperationalError, connect
+
+import instaloader as il
+
+
+def import_session(loader):
+    # Find firefox cookies
+    default_cookiefile = {
+        "Windows": "~/AppData/Roaming/Mozilla/Firefox/Profiles/*/cookies.sqlite",
+        "Darwin": "~/Library/Application Support/Firefox/Profiles/*/cookies.sqlite",
+    }.get(system(), "~/.mozilla/firefox/*/cookies.sqlite")
+    cookiefile = glob(expanduser(default_cookiefile))[0]
+
+    # Connect and find insta session
+    conn = connect(f"file:{cookiefile}?immutable=1", uri=True)
+    try:
+        cookie_data = conn.execute(
+            "SELECT name, value FROM moz_cookies WHERE baseDomain='instagram.com'"
+        )
+    except OperationalError:
+        cookie_data = conn.execute(
+            "SELECT name, value FROM moz_cookies WHERE host LIKE '%instagram.com'"
+        )
+
+    # Set session
+    loader.context._session.cookies.update(cookie_data)
+    username = loader.test_login()
+    if not username:
+        raise SystemExit("Not logged in. Are you logged in successfully in Firefox?")
+    loader.context.username = username
+    loader.save_session_to_file()
+
+    return username
+
+
+# Create instance
+loader = il.Instaloader(
+    download_video_thumbnails=False,
+    save_metadata=False,
+    filename_pattern="{date_utc}",
+    post_metadata_txt_pattern="",
+    max_connection_attempts=1,
+)
+
+# Import session from firefox login
+user = import_session(loader)
+
+# Update path and download
+loader.dirname_pattern = f"out/instagram/{user}/saved/"
+loader.download_saved_posts()
