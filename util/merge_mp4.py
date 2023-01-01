a60d81227218e1da8f544c4c2a8c0e1040950bf7
Marek Hummel <marek.hummel3@gmail.com>
2023-01-01 23:15:41 +0100

    Updated mp4 merge

--- util/merge_mp4.py
+++ util/merge_mp4.py
@@ -5,11 +5,12 @@ from os import mkdir, rename
 # ------
 
 path = r"D:\UserFolders\Downloads\dwhelper\\"
-title = "Intelligenz"
-# mkdir(path + title)
+title = "Das Einmaleins für Finanzen"
+mkdir(path + title)
+mkdir(path + title + "\\Lektionen")
 
 
-for i in range(14):
+for i in range(9):
     video = f"»{title}« Die ZEIT Akademie - Seminar Player www.zeitakademie.de-{i*2}.mp4"
 
     if i == 0:
@@ -17,4 +18,13 @@ for i in range(14):
     audio = f"»{title}« Die ZEIT Akademie - Seminar Player www.zeitakademie.de-{i*2 + 1}.mp4"
     target = f"{title} - Lektion {i+1:02d}.mp4"
     print("Merge:", target)
-    ffmpeg_merge_video_audio(path + video, path + audio, path + title + "\\" + target, logger=None)
+    ffmpeg_merge_video_audio(path + video, path + audio, path + title + "\\Lektionen\\" + target, logger=None)
+
+# ------
+
+
+# for i in range(13, 21, +1):
+#     file = f"»{title}« Die ZEIT Akademie - Seminar Player www.zeitakademie.de-{i}.mp4"
+#     new_file = f"»{title}« Die ZEIT Akademie - Seminar Player www.zeitakademie.de-{i-1}.mp4"
+
+#     rename(path + file, path + new_file)
