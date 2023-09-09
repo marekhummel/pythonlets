f6060d79f5bd87bc257081cfff99b1a97f8e80da
Marek Hummel <marek.hummel3@gmail.com>
2023-09-09 21:43:18 +0200

    Update typing backup script

--- validate_backup/main.py
+++ validate_backup/main.py
@@ -7,42 +7,46 @@ import os
 import os.path
 import shutil
 from time import perf_counter
+from pathlib import Path
+from typing import Any, Iterator
 
-DATA_PATH = r"D:\UserFolders\Backup"
-BACKUP_PATH = r"D:\OneDrive\Pictures"
+DATA_PATH = Path(r"D:\OneDrive\Phone Media\WhatsApp")
+BACKUP_PATH = Path(r"D:\OneDrive\Pictures")
 IGNORED = []  # ["FOUND.000", "System Volume Information", ".Spotlight-V100"]
 
 
-def get_file_iterator(root):
-    return (os.path.join(currentpath, f) for currentpath, _, files in os.walk(root) for f in files)
+def get_file_iterator(root: Path) -> Iterator[Path]:
+    return (Path(currentpath) / f for currentpath, _, files in os.walk(root) for f in files)
 
 
-def get_hashdict(file_path, hashlist_path):
-    print("Update hashdict")
+def get_hashdict(file_path: Path, hashlist_path: Path) -> set[str | None]:
+    print(f"Update hashdict of {file_path}")
     all_files = list(get_file_iterator(file_path))
 
-    hashlist = {}
-    if os.path.exists(hashlist_path):
-        with open(hashlist_path, "r") as f:
-            hashlist = json.load(f)
+    hashlist: dict[Path, str | None] = {}
+    if hashlist_path.exists():
+        with hashlist_path.open("r") as f:
+            hashes = json.load(f)
+            hashlist = {Path(p): h for p, h in hashes.items()}
 
     with cf.ProcessPoolExecutor() as executor:
         new_files = set(all_files) - set(hashlist.keys())
-        for f, file_hash in zip(new_files, executor.map(compute_file_hash, new_files)):
-            hashlist[f] = file_hash
+        for file, file_hash in zip(new_files, executor.map(compute_file_hash, new_files)):
+            hashlist[file] = file_hash
 
     print(f"Created hashlist with {len(hashlist)} entries")
-    with open(hashlist_path, "w", encoding="utf-8") as f:
-        json.dump(hashlist, f)
+    with hashlist_path.open("w", encoding="utf-8") as f:
+        json.dump({str(p): h for p, h in hashlist.items()}, f)
+
     return set(hashlist.values())
 
 
-def compute_file_hash(file):
+def compute_file_hash(file: Path) -> str | None:
     BLOCK_SIZE = 65536
 
     file_hash = hashlib.md5()
     try:
-        with open(file, "rb") as f:
+        with file.open("rb") as f:
             fb = f.read(BLOCK_SIZE)
             while len(fb) > 0:
                 file_hash.update(fb)
@@ -54,20 +58,20 @@ def compute_file_hash(file):
         return None
 
 
-def check_data(path, hashset, ignored):
+def check_data(data_path: Path, hashset: set[str | None], ignored: list[str]) -> list[Path]:
     print("Check files")
-    abs_ignored = [os.path.join(path, i) for i in ignored]
+    abs_ignored = [os.path.join(data_path, i) for i in ignored]
 
     total = 0
     not_found = []
     not_found_count = 0
     with cf.ProcessPoolExecutor() as executor:
-        for currentpath, _, files in os.walk(path):
+        for currentpath, _, files in os.walk(data_path):
             if currentpath in abs_ignored:
                 print(f" - {currentpath} is skipped")
                 continue
 
-            fullfiles = [os.path.join(currentpath, f) for f in files]
+            fullfiles = [Path(currentpath) / f for f in files]
             total += len(fullfiles)
 
             hashed_files = [
@@ -77,7 +81,8 @@ def check_data(path, hashset, ignored):
 
             not_found_count += len(current_not_found)
             if len(current_not_found) == len(hashed_files) and current_not_found:
-                not_found.append(f"{currentpath}\\*.*")
+                # not_found.append(f"{currentpath}\\*.*")
+                not_found.append(Path(currentpath))
             else:
                 not_found.extend(current_not_found)
 
@@ -101,23 +106,23 @@ def copy_not_found(root, files, check_path):
             shutil.copy2(f, target)
 
 
-def group_files(files):
+def group_files(files: list[str]) -> dict:
     split_files = [f.rstrip().split("\\", 1) for f in files]
 
-    groups = {}
-    files = []
+    groups: dict[str, list[str]] = {}
+    curr_files: list[str] = []
     for f in split_files:
         if len(f) == 1:
-            files.append(f[0])
+            curr_files.append(f[0])
         else:
             grp = f[0]
             if grp not in groups:
                 groups[grp] = []
             groups[grp].append(f[1])
 
-    hierarchy = {}
-    if files:
-        hierarchy["%%files%%"] = files
+    hierarchy: dict = {}
+    if curr_files:
+        hierarchy["%%files%%"] = curr_files
     for g, vals in groups.items():
         hierarchy[g] = group_files(vals)
 
@@ -127,9 +132,9 @@ def group_files(files):
 if __name__ == "__main__":
     # Start
     start = perf_counter()
-    hash_path = r".\validate_backup\hashes.json"
-    target_path = r".\validate_backup\not_found.json"
-    check_path = r".\validate_backup\check\\"
+    hash_path = Path(r".\validate_backup\hashes.json")
+    target_path = Path(r".\validate_backup\not_found.json")
+    check_path = Path(r".\validate_backup\check\\")
 
     # Check paths
     if os.path.exists(check_path):
@@ -144,7 +149,7 @@ if __name__ == "__main__":
     # copy_not_found(DATA_PATH, files, check_path)
 
     # Create lookup
-    hierarchy = group_files(files)
+    hierarchy = group_files([str(p) for p in files])
     with open(target_path, mode="w", encoding="utf-8") as f:
         json.dump(hierarchy, f, indent=4, separators=(", ", ": "), sort_keys=True)
 
