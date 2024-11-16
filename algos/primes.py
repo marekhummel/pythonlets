4b0d0e417ddefb6aaedc04ba25f49566b5511e04
Marek Hummel <marek.hummel3@gmail.com>
2024-11-16 23:45:00 +0100

    Added atkins sieve (primes)

--- algos/primes.py
+++ algos/primes.py
@@ -1,3 +1,4 @@
+from itertools import count
 from math import sqrt
 from typing import Iterator, List, Tuple
 
@@ -17,7 +18,7 @@ def euler_totient_sieve(n: int) -> List[int]:
     return phi
 
 
-def prime_sieve(n):
+def prime_sieve_eratostenes(n):
     """Sieve of Eratostenes up to n"""
     root = int(sqrt(n))
     primes = [True] * (n + 1)
@@ -28,6 +29,84 @@ def prime_sieve(n):
             primes[i * i : n + 1 : i] = [False] * m
 
     return (i for i, p in enumerate(primes) if p)
+
+
+def prime_sieve_atkins(limit: int):
+    """Sieve of Atkins up to n"""
+    is_prime = [False] * limit
+    s = [1, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 49, 53, 59]
+
+    n_mod_60_1 = {1, 13, 17, 29, 37, 41, 49, 53}
+    for x in count(1):
+        x2 = 4 * x * x
+        if x2 >= limit:
+            break
+        for y in count(1, step=2):
+            n = x2 + y * y
+            if n >= limit:
+                break
+            if n % 60 in n_mod_60_1:
+                is_prime[n] = not is_prime[n]
+
+    n_mod_60_2 = {7, 19, 31, 43}
+    for x in count(1, step=2):
+        x2 = 3 * x * x
+        if x2 >= limit:
+            break
+        for y in count(2, step=2):
+            n = x2 + y * y
+            if n >= limit:
+                break
+            if n % 60 in n_mod_60_2:
+                is_prime[n] = not is_prime[n]
+
+    n_mod_60_3 = {11, 23, 47, 59}
+    for x in count(2, step=1):
+        x2 = 3 * x * x
+        if 2 * x * x + 2 * x - 1 >= limit:
+            break
+        for y in range(x - 1, 0, -2):
+            n = x2 - y * y
+            if n >= limit:
+                break
+            if n % 60 in n_mod_60_3:
+                is_prime[n] = not is_prime[n]
+
+    for w in count(0):
+        if 3600 * w * w >= limit:
+            break
+        for x in s:
+            n = 60 * w + x
+            if n < 7:
+                continue
+
+            n2 = n * n
+            if n2 >= limit:
+                break
+
+            if is_prime[n]:
+                for w2 in count(0):
+                    if n2 * (60 * w2 * s[0]) >= limit:
+                        break
+                    for x2 in s:
+                        c = n2 * (60 * w2 + x2)
+                        if c >= limit:
+                            break
+
+                        is_prime[c] = False
+
+    yield from [2, 3, 5]
+    for w in count(0):
+        if 60 * w >= limit:
+            break
+        for x in s:
+            n = 60 * w + x
+            if n < 7:
+                continue
+            if n > limit:
+                break
+            if is_prime[n]:
+                yield n
 
 
 def naive_is_prime(n: int) -> bool:
@@ -120,3 +199,10 @@ def prime_factors(n: int) -> Iterator[Tuple[int, int]]:
         p += 1 if p == 2 else 2
     if n > 1:
         yield n, 1
+
+
+if __name__ == "__main__":
+    n = 10000
+    primes1 = prime_sieve_eratostenes(n)
+    primes2 = prime_sieve_atkins(n)
+    assert set(primes1) == set(primes2)
