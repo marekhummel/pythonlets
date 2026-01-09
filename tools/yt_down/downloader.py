f7c9e112b15901fc39b3b20dbc140128b6b50eec
Marek Hummel <marek.hummel3@gmail.com>
2026-01-09 21:37:16 +0100

    Updated yt downloader

--- tools/yt_down/downloader.py
+++ tools/yt_down/downloader.py
@@ -1,45 +1,35 @@
 import re
 import shutil
-from os import walk
-from os.path import splitext
 
-from pytube import Playlist, YouTube
-from pytube.helpers import safe_filename
+from pytubefix import Playlist, YouTube
 
 
-def download_playlist(uri, target, ignore_rgx):
+def download_playlist(uri, target, audio_only, ignore_rgx):
     pl = Playlist(uri)
     pl._video_regex = re.compile(r"\"url\":\"(/watch\?v=[\w-]*)")
     for link in pl:
-        download_video(link, target, ignore_rgx)
+        download_video(link, target, audio_only, ignore_rgx)
 
 
-def download_video(uri, target, ignore_rgx="(?!)"):
+def download_video(uri, target, audio_only=False, ignore_rgx="(?!)"):
     cols = shutil.get_terminal_size()[0] - 1
 
-    tries = 0
-    while tries < 5:
-        try:
-            print(f'Attempting "{uri}".', end="\r")
-            yt = YouTube(uri)
-            all_files = rec_files(target)
-            if re.match(ignore_rgx, yt.title):
-                print(f'"{yt.title}" ignored.'.ljust(cols))
-                return
-            if safe_filename(yt.title) in all_files:
-                print(f'"{yt.title}" skipped.'.ljust(cols))
-                return
-            print(f'"{yt.title}" downloading...'.ljust(cols), end="\r")
-            yt.streams.get_highest_resolution().download(target)
-            print(f'"{yt.title}" done.'.ljust(cols))
-            break
-        except:  # noqa
-            tries += 1
-    else:
-        print(f'"{uri}" failed.'.ljust(cols))
+    try:
+        print(f'Attempting "{uri}".', end="\r")
+        yt = YouTube(uri)
 
+        if re.match(ignore_rgx, yt.title):
+            print(f'"{yt.title}" ignored.'.ljust(cols))
+            return
 
-def rec_files(path):
-    for _, _, filenames in walk(path):
-        for filename in filenames:
-            yield splitext(filename)[0]
+        print(f'"{yt.title}" downloading...'.ljust(cols), end="\r")
+
+        if audio_only:
+            stream = yt.streams.filter(only_audio=True).order_by("abr").desc().first()
+        else:
+            stream = yt.streams.get_highest_resolution()
+        stream.download(target, skip_existing=True, max_retries=3)
+
+        print(f'"{yt.title}" done.'.ljust(cols))
+    except Exception as e:  # noqa
+        print(f'"{uri}" failed ({e}).'.ljust(cols))
--- tools/yt_down/main.py
+++ tools/yt_down/main.py
@@ -1,14 +1,18 @@
 import argparse
+import sys
 
 from downloader import download_playlist, download_video
 
 if __name__ == "__main__":
-    # sys.argv.append('-p')
-    # sys.argv.append('PLhlJzNE93TpbeOEXwji2SWu62wMmifFJI')
+    # sys.argv.append("-v")
+    # sys.argv.append("1sM89qLmd9Y")
+    sys.argv.append("-p")
+    sys.argv.append("PLChB_B712tZg2clVHoZjEsikOS4BeVryE")
     # sys.argv.append('-i')
     # sys.argv.append(r'Integrieren|Koordinatensysteme')
-    # sys.argv.append('-o')
-    # sys.argv.append(r'.')
+    sys.argv.append("-o")
+    sys.argv.append(r"./_out/yt/")
+    sys.argv.append("-a")
 
     parser = argparse.ArgumentParser(description="YouTube Downloader")
     group = parser.add_mutually_exclusive_group(required=True)
@@ -16,16 +20,18 @@ if __name__ == "__main__":
     group.add_argument("-v", "--video", type=str)
     parser.add_argument("-i", "--ignore", type=str, default="(?!)")
     parser.add_argument("-o", "--output", type=str)
+    parser.add_argument("-a", "--audio-only", action="store_true")
 
     args = parser.parse_args()
+    print(args)
 
     if args.video:
         uri = f"https://www.youtube.com/watch?v={args.video}"
-        target = args.output if args.output else "./out/"
-        download_video(uri, target)
+        target = args.output if args.output else "./_out/yt"
+        download_video(uri, target, args.audio_only)
         print("File downloaded.")
     elif args.playlist:
         uri = f"https://www.youtube.com/playlist?list={args.playlist}"
-        target = args.output if args.output else f"./out/{args.playlist}"
-        download_playlist(uri, target, args.ignore)
+        target = args.output if args.output else f"./_out/yt/{args.playlist}"
+        download_playlist(uri, target, args.audio_only, args.ignore)
         print("\r\nFile(s) downloaded.\r\nDone.")
