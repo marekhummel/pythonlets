8c30d446b553fcbe99752a5d3d63986ead4a07a4
Marek Hummel <marek.hummel3@gmail.com>
2026-01-31 17:55:47 +0100

    Updated BBP

--- algos/pi/bbp.py
+++ algos/pi/bbp.py
@@ -1,8 +1,13 @@
 # BBP Formula
 
+import math
 from decimal import Decimal, getcontext
 
 getcontext().prec = 100
+
+pi_exact = Decimal(
+    "3.1415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679"
+)
 
 pi = Decimal(0)
 
@@ -15,4 +20,5 @@ for n in range(50):
 
     pi += (t1 - t2 - t3 - t4) * s
 
-print(pi)
+    prec = math.ceil(math.log10(abs(pi - pi_exact) / pi_exact))
+    print(n, round(pi, -prec + 1), prec)
